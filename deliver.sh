6ad3685fe0a3d77f552c8f62b826333877a9e5dc
Diez B. Roggisch <deets@web.de>
2013-07-05 17:26:49 +0200

    How to bundle and ship

---
+++ deliver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME=Minimal-0.1
+PYTHON=/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7
+
+rm -rf dist build
+
+$PYTHON setup.py py2app 
+
+if [ -e $NAME.dmg ]; then
+    rm $NAME.dmg
+fi
+hdiutil create -volname $NAME -srcdir dist $NAME.dmg
